c436816672406fb324cb2bb8196617eda5221558
root <root@ip-172-31-23-37.ap-southeast-1.compute.internal>
2022-02-27 15:10:43 +0000

    quyan

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Annnnnnnnnnnnnnn");
+		get("/hello", (req, res) -> "Quyandeptrai");
 	}
 }
